1586a26298c1627d5ffd5c274f859a5c245a4468
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-07-14 20:41:38 -0400

    Update spin-ec2-windows.sh
    Added region to AWS commands

--- spin-ec2-windows.sh
+++ spin-ec2-windows.sh
@@ -40,12 +40,12 @@ name="Your_Name"
 windows_username="Administrator"
 
 instance_id=$(aws ec2 run-instances --image-id $image_id --count $count --instance-type $instance_type \
---key-name $key_name --security-group-ids $security_group | jq -r '.Instances[0].InstanceId')
+--key-name $key_name --security-group-ids $security_group --region $region | jq -r '.Instances[0].InstanceId')
 
 echo "The instance ID is $instance_id."
 echo $instance_id > ~/.instance_id
 
-public_dns_name=$(aws ec2 describe-instances --instance-ids ${instance_id} \
+public_dns_name=$(aws ec2 describe-instances --region $region --instance-ids ${instance_id} \
 --query 'Reservations[].Instances[].PublicDnsName' --output text)
 
 echo "The public DNS name is $public_dns_name."
@@ -56,7 +56,7 @@ echo "Meanwhile, here are some quotes about 💙💙💙"
 password_status=false
 while ! $password_status
 do
-    get_password=$(aws ec2 get-password-data --instance-id $instance_id \
+    get_password=$(aws ec2 get-password-data --region $region --instance-id $instance_id \
                     --priv-launch-key $pem_location/$key_name.pem | jq -r '.PasswordData')
     get_random_quote
     if [[ -n $get_password ]]
